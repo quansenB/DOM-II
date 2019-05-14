4bf8d5793661de37113950d5d2c53a902b0a2849
quansenB <inaki.breinbauer@gmail.com>
2019-05-14 17:17:06 +0200

    finish task

--- js/index.js
+++ js/index.js
@@ -1,1 +1,68 @@
 // Your code goes here
+let imgfunbus = document.querySelector(".intro img");
+
+imgfunbus.addEventListener("click", (event) => {
+    event.target.style.display = "none";
+});
+
+imgfunbus.addEventListener("mouseover", (event) => {
+    event.target.style.width = "50%";
+});
+
+imgfunbus.addEventListener("mouseout", (event) => {
+    event.target.style.width = "100%";
+});
+
+
+let logo = document.querySelector(".logo-heading");
+
+logo.addEventListener("mouseover", (event) => {
+    event.target.style.transform = "rotate(-45deg)";
+});
+
+logo.addEventListener("dblclick", (event) => {
+    event.target.style.transform = "rotate(180deg)";
+});
+
+
+let h2s = document.querySelectorAll("h2");
+
+h2s.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+        event.target.style.color = "blue";
+    });
+});
+
+
+let page = document;
+
+page.addEventListener("wheel", (event) => {
+    event.target.style.backgroundColor = "lightgrey";
+});
+
+page.addEventListener("keydown", (event) => {
+    event.target.style.backgroundColor = "grey";
+});
+
+
+let buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        event.target.style.transform = "rotate(45deg)";
+    });
+});
+
+buttons.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "red";
+    });
+});
+
+let header = document.querySelector(".nav-container");
+header.addEventListener("mouseover", (event) => {
+    event.target.style.transform = "rotate(185deg)";
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    /* event.preventDefault(); */
+});
